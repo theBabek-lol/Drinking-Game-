04012fb597f08d71b4fad0e982e9cadd0af31718
theBabek-lol <dlundalv@gmail.com>
2025-09-03 18:59:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', async () => {
-    const APP_VERSION = "1.0.2"; // bumpa när du deployar
+    const APP_VERSION = "1.0.3"; // bumpa när du deployar
 
     // --- Version label ---
     const versionEl = document.createElement("div");
