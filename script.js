146ba459e2b08672afa58a7d34866a3ca0277581
theBabek-lol <dlundalv@gmail.com>
2025-09-14 13:00:20 +0000

    change sliders visual

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', async () => {
-    const APP_VERSION = "1.0.7"; // bumpa när du deployar
+    const APP_VERSION = "1.0.8"; // bumpa när du deployar
 
     // --- Version label ---
     const versionEl = document.createElement("div");
