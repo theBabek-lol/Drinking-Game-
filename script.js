ead11205c9719c9f138b39b1d9dc2ad6e5ed8072
theBabek-lol <dlundalv@gmail.com>
2025-09-15 14:35:01 +0000

    fixed new gamebutton

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', async () => {
-    const APP_VERSION = "1.3.5"; // bumpa när du deployar
+    const APP_VERSION = "1.3.6"; // bumpa när du deployar
 
     // --- Version label ---
     const versionEl = document.createElement("div");
