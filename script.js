00673a5d504def9558dea5d209196c84a6531c4c
theBabek-lol <dlundalv@gmail.com>
2025-09-15 15:38:54 +0000

    button leak issue fix

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', async () => {
-    const APP_VERSION = "1.3.11"; // bumpa när du deployar
+    const APP_VERSION = "1.3.12"; // bumpa när du deployar
 
     // --- Version label ---
     const versionEl = document.createElement("div");
