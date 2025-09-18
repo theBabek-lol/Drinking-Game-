f9542ac64373f4add40b8536e2b135ef723d908c
theBabek-lol <dlundalv@gmail.com>
2025-09-18 11:18:00 +0000

    card animation

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', async () => {
-    const APP_VERSION = "1.5.3"; // bumpa när du deployar
+    const APP_VERSION = "1.5.4"; // bumpa när du deployar
     
     // --- Cache busting ---
     document.querySelectorAll('link[rel="stylesheet"], script[src]').forEach(el => {
