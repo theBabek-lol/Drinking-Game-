45e0a7a494033785df2d5faf689fc04bdd464a8e
theBabek-lol <dlundalv@gmail.com>
2025-09-18 15:15:15 +0000

    changed text from white=)

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', async () => {
-    const APP_VERSION = "1.5.23"; // bump version on deploy
+    const APP_VERSION = "1.5.24"; // bump version on deploy
 
     // --- Cache busting ---
     document.querySelectorAll('link[rel="stylesheet"], script[src]').forEach(el => {
