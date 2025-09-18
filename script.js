28777015cafe9b15699e6c1749ad9c68ff0d80c6
theBabek-lol <dlundalv@gmail.com>
2025-09-18 12:01:51 +0000

    fixes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', async () => {
-    const APP_VERSION = "1.5.6"; // bumpa när du deployar
+    const APP_VERSION = "1.5.7"; // bumpa när du deployar
     
     // --- Cache busting ---
     document.querySelectorAll('link[rel="stylesheet"], script[src]').forEach(el => {
