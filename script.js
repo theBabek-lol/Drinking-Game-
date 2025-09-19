229b63d13b14432fd73491a5acf6f954a7c99f3b
theBabek-lol <dlundalv@gmail.com>
2025-09-19 08:56:23 +0000

    fixed large text cards

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', async () => {
-    const APP_VERSION = "1.5.34"; // bump version on deploy
+    const APP_VERSION = "1.5.35"; // bump version on deploy
 
     // --- Cache busting ---
     document.querySelectorAll('link[rel="stylesheet"], script[src]').forEach(el => {
