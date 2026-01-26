632923bc555f0e8bc96864b35cb6b76ce9d34928
theBabek-lol <dlundalv@gmail.com>
2026-01-26 12:07:10 +0100

    sahre button fix

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', async () => {
-    const APP_VERSION = "2.1.0"; // bump version on deploy
+    const APP_VERSION = "2.1.1"; // bump version on deploy
 
     // --- Cache busting ---
     document.querySelectorAll('link[rel="stylesheet"], script[src]').forEach(el => {
